d1fc07509aea60188d0f53d26c2dcd62c8fd14ff
Oleksiy Tarlovskyy <tarlovsky@gmail.com>
2017-12-07 10:29:14 +0000

    Documentation in routes

--- routes.js
+++ routes.js
@@ -32,6 +32,7 @@ module.exports = function(app){
         })
     })
     
+    //USER HAS TO BE LOGGED IN TO ATTACH A DEVICE TO HIMSELF
     app.get('/attach-device', ensureLoggedIn, function (req, res, next) {
         
             var User = require('./models/user'),
@@ -41,14 +42,20 @@ module.exports = function(app){
                 crypto = require('crypto'),
                 qrimage = require('qr-image')
                 ;
+
             var u = req.session.user;
             
+            //GENERATE SECTET KEY
             var secretKey = utils.randomKey(48);
-            
+            //GENERATE THE FIRST EVER CHALLENGE
             const challenge = utils.getChallenge();
-            var pin_len = utils.randomInt(6,9);
+            var pin_len = utils.randomInt(6,9);//EXPECTED PIN LENGTH
+            //GET'S PASSCODE OF pin_len SIZE FROM A GENERATED KEYED HMAC
+            //ONLY THIS DEVICE'S KEY IS ABLE TO GENERATE THE SAME HMAC FOR A GIVEN CHALLENGE
             const awaited_answer = utils.passcodeGenerator(secretKey, challenge, pin_len);
             
+            //CREATE THE ACTUAL DEVICE
+            //ENCRYPT KEY INSIDE DB
             var deviceToInsert = new Device({
                 deviceName: "My First Authentication Device",
                 key: utils.encrypt(secretKey),
@@ -56,22 +63,26 @@ module.exports = function(app){
                 confirmed: false,
                 first_awaited_answer: awaited_answer.toString()
             });
-            
+            //MONGO WORKS DIFFERENT FROM TRADITIONAL MYSQL
+            //IN A SENSE THAT WE DON'T HAVE TO FETCH THE last_inserted_id   
+            //IT'S ALWAYS WITH US
             var did = deviceToInsert._id;
             
             deviceToInsert.save(function(err) {
                 if (err){throw err;}
             });
 
-            // Generate qr for the user's device
+            // Generate QR for the user's device
             var otpUrl = 'otpauth://hotp/'+ 'passepartout:' + u.email + 
             '?secret=' + secretKey + 
             '&challange=' + challenge + 
             '&issuer=passepartout' + 
             '&pinlength=' + pin_len;
             
+            //PNG IMAGE BUFFER
             var qr_image_data = new Buffer(qrimage.imageSync(otpUrl, {type:'png'})).toString('base64');
             
+            //RENDER THE VIEW
             res.render('attach-device', {
                 layout: 'main',
                 
@@ -79,13 +90,12 @@ module.exports = function(app){
                 user: u,
                 time_limit: MAX_WAIT_2FA_ATTACH,
                 //nao passamos key
-                //passamos devid da base de dados. Registo ainda e unconfirmed
-                //e user deve ser checked quando fazemos 
+                //passamos device_id 
                 //secretKey: secretKey, //reduce key usage, certainly in forms sent to client.
                 qr: qr_image_data,
                 devid: did,
-                //REMOVE PRODUCTION
-                //USE ON DEBUG
+                
+                //REMOVE IN PRODUCTION
                 //answer: awaited_answer
             })
         
@@ -94,9 +104,10 @@ module.exports = function(app){
     app.post('/do-attach-device', ensureLoggedIn, function(req, res){
         var user = req.session.user;
         if(user){
-            //var Device = require('./models/device')
-            // TODO implement csrf token protection
+
+            // TODO implement csrf token protection FOR ATTACH AND DETATCH
             // var _csrf = req.body._csrf;
+
             var current_user_id = user._id;
             var current_user_email = user.email;
             
@@ -111,12 +122,13 @@ module.exports = function(app){
                     
                     //the user had one minute to do this
                     if( (user_answer == obj.first_awaited_answer) && ((new Date() - new Date(obj.created_at)) < (MAX_WAIT_2FA_ATTACH)) ){
+                        //USER'S ANSWER WAS VALID AND INSIDE THE TIME FRAME
                         Device.update({ _id: did, uid: current_user_id  }, { $set: { confirmed: true, deviceName: device_name } }, function(){
                             req.flash('deviceAttachMesage', 'Device ' + device_name + ' registered sucessefully!')
                             res.redirect(303, '/user-account');
                         });
                     }else{
-                        //ask user to start over because he missed the window
+                        //ask user to start over because he missed the window or passcode was wrong
                         Device.remove({_id: did, uid: current_user_id}).exec(function(err){
                             if(err){throw err;}
                             req.flash('deviceAttachMesage', 'Looks like you were too late or the answer was wrong, try again but this time faster!' );
@@ -137,11 +149,13 @@ module.exports = function(app){
         res.render('register', { message: req.flash('signupMessage'), layout: 'layout-sign-in' })
     })
     
-    // process the signup form
+    // PROCESS THE SIGNUP FORM
     app.post('/do-register-user', function(req, res){
         var email = req.body.email,
             password = req.body.password,
-            rpassword = req.body.repeatpassword;
+            rpassword = req.body.repeatpassword
+        ;
+
         if(password && rpassword && email){
             if(password != rpassword){
                 req.flash('signupMessage', 'Passwords do not match.');
@@ -174,14 +188,15 @@ module.exports = function(app){
             });
         }
         
-
     });
     
+
     app.post('/do-sign-in', function(req, res){
         
         User.authenticate(req.body.email, req.body.password, function(error, result, info){
             if(result){
-                if(info != null && typeof info == 'string'){//info contains 2fa method name
+                //INFO RETURNS THE SECOND FACTOR METHOD NAME, EX.: hotp
+                if(info != null && typeof info == 'string'){
                     req.session.twoFactorPending = info
                     req.session.pendingUser = result
                     res.redirect('/device-choice')
@@ -203,12 +218,13 @@ module.exports = function(app){
         res.render('sign-in', { layout: 'layout-sign-in', message: req.flash('loginMessage')})
     })
    
-    // Logout endpoint
     app.post('/sign-out', function (req, res) {
         req.session.destroy();
         res.redirect('/');
     });
     
+    // LIST ALL USER'S DEVICES
+    // ONLY ENTER HERE IF YOU'VE GOT A PENDING LOGIN FROM ENTERING <USERNAME, PASSWORD>
     app.get('/device-choice', ensurePendingUser, function(req, res){
         var method = req.session.twoFactorPending;//generally totp
         var userObject = req.session.pendingUser;
@@ -219,7 +235,8 @@ module.exports = function(app){
             })
         }
     })
-
+    
+    //REMOVE DEVICE FROM DB THUS DETATCHING IT FROM USER
     app.post('/detatch-device', ensureLoggedIn, function(req, res){
         Device.findOne({_id: req.body.devid}, function(err, d){
             if(d.uid.toString() == req.session.user._id.toString()){
@@ -238,24 +255,34 @@ module.exports = function(app){
         var devid = req.body.devid;
 
         Device.findOne({uid: userObject._id, _id: devid}, function(err, dev){
-            
+            /**
+             * REPLICATE WHAT THE DEVICE WOULD GENERATE AT LOGIN
+             */
+            //DECRYPT THE KEY STORED IN THE DB
             var deviceKey = utils.decrypt(dev.key);
-
+            //GENERATE A CHALLENGE FOR THE USER
             const challenge = utils.getChallenge();
-
+            //EXPECTED PIN LENGTH
             var pin_len = utils.randomInt(6,9)
-
+            //ONLY THIS DEVICE'S KEY IS ABLE TO GENERATE THE SAME HMAC FOR A GIVEN CHALLENGE
             const awaited_answer = utils.passcodeGenerator(deviceKey, challenge, pin_len);
-            // Generate qr for the screen
-            var otpUrl = 'otpauth://hotp/'+ 'programist:' + req.session.pendingUser.email + 
+            // EVERYTHING BUT THE KEY INTO THE QR
+            var otpUrl = 'otpauth://hotp/'+ 'passepartout:' + req.session.pendingUser.email + 
             '?challange=' + challenge + 
-            '&issuer=programist' + 
+            '&issuer=passepartout' + 
             '&pinlength=' + pin_len;
+            
+            //PNG IMAGE BUFFER
             const qr_image_data = new Buffer(qrimage.imageSync(otpUrl, {type:'png'})).toString('base64');
 
+            //Save the token into the db
             var t = new Token({
                 challange: challenge,
-                awaitedAnswer: awaited_answer,//must hide this in hashed form with some salt and key please.
+                
+                //THIS IS NOT STORED IN PLAINTEXT,
+                //REFER TO THE TOKEN.JS FILE IN ./models
+                awaitedAnswer: awaited_answer,
+
                 did: dev._id,
                 uid: userObject._id,
             })
@@ -266,6 +293,7 @@ module.exports = function(app){
                 }
             })
             
+            //render content
             res.render('two-factor-login', {
                 layout: 'layout-sign-in', 
                 email: userObject.email, 
@@ -273,7 +301,8 @@ module.exports = function(app){
                 devname: dev.deviceName,
                 qr: qr_image_data,
                 tokenid : t._id,
-                //remove after tests
+
+                //REMOVE AT PRODUCTION
                 //answer: awaited_answer
             });
         })
@@ -287,10 +316,9 @@ module.exports = function(app){
         Token.findOne({_id: tid}, function(err, t){
             if(err){throw err;}
             
-
             if( t.validateAnswer(ans.toString()) ){
                 if(new Date() - t.created_at < MAX_WAIT_2FA_AUTH){
-
+                    //USER'S ANSWER WAS VALID AND INSIDE THE TIME FRAME
                     req.session.user = req.session.pendingUser;
                     delete req.session.pendingUser;
                     req.flash('deviceAttachMesage', 'Congrats, You have sucessefully authenticated with your device!')
@@ -312,6 +340,11 @@ module.exports = function(app){
     })
 };
 
+/**
+ * 
+ * Logged in guard
+ * Don't render content withought passing it
+ */
 function ensureLoggedIn(req, res, next) {
     if (req.session.user) {
       return next();
@@ -322,6 +355,9 @@ function ensureLoggedIn(req, res, next) {
     }
 }
 
+// Ensure pending user guard prevents users 
+// from manually typing in a link and skipping 
+// the Second factor step
 function ensurePendingUser(req, res, next){
     if(req.session.pendingUser){
         return next();
